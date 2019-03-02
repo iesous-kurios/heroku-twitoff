1006f883d422f4c1d28fb00e5916eae22f275c27
soycode <soycode@users.noreply.github.com>
2019-03-01 22:13:37 -0800

    Basic tweet/user models with relationship between them

--- tweetometer/models.py
+++ tweetometer/models.py
@@ -1,9 +1,24 @@
 """Models for Tweetometer, made with SQLAlchemy."""
 from flask_sqlalchemy import SQLAlchemy
+from . import APP
 
-DB = SQLAlchemy(app)
+DB = SQLAlchemy(APP)
 
 
-class TwitterUser(DB.Model):
-    username = DB.Column(DB.String(50))
+class User(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(50), nullable=False)
     last_fetched = DB.Column(DB.DateTime)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
+
+
+class Tweet(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    text = DB.Column(DB.String(200))
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
