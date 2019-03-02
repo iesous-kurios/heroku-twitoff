00e20385cbaee1d62a81c52b91e8ff4c8a167b25
soycode <soycode@users.noreply.github.com>
2019-03-02 10:08:40 -0800

    Combine Twitter and Basilica logic in same file, tweak DB/utilities

--- twitoff/models.py
+++ twitoff/models.py
@@ -1,4 +1,4 @@
-"""Models for TwitOff, made with SQLAlchemy."""
+"""SQLAlchemy models and utility functions for TwitOff."""
 from flask_sqlalchemy import SQLAlchemy
 from . import APP
 
@@ -6,8 +6,9 @@ DB = SQLAlchemy(APP)
 
 
 class User(DB.Model):
+    """Twitter users corresponding to Tweets in the Tweet table."""
     id = DB.Column(DB.Integer, primary_key=True)
-    name = DB.Column(DB.String(50), nullable=False)
+    name = DB.Column(DB.String(15), nullable=False)
     last_fetched = DB.Column(DB.DateTime)
 
     def __repr__(self):
@@ -15,10 +16,18 @@ class User(DB.Model):
 
 
 class Tweet(DB.Model):
+    """Tweets and their embeddings from Basilica."""
     id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.String(200))
+    text = DB.Column(DB.Unicode(280))  # Future-proofing, API is short/ascii
+    embeddings = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
         return '<Tweet {}>'.format(self.text)
+
+
+def reset_db():
+    """Drop and recreate all database tables."""
+    DB.drop_all()
+    DB.create_all()
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -1,15 +1,22 @@
-"""Retrieve data from Twitter using Tweepy."""
+"""Retrieve Tweets, embeddings, and persist in the database."""
+import basilica
 import tweepy
 from decouple import config
 
-AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'),
-                           config('TWITTER_CONSUMER_SECRET'))
-AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),
-                      config('TWITTER_ACCESS_TOKEN_SECRET'))
+TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'),
+                                   config('TWITTER_CONSUMER_SECRET'))
+TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),
+                              config('TWITTER_ACCESS_TOKEN_SECRET'))
+TWITTER = tweepy.API(TWITTER_AUTH)
+BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
-API = tweepy.API(AUTH)
 
-
-def user_tweets(user):
-    tweets = API.get_user(user).timeline()
+def user_tweets(user, count=200):
+    """Return default 200 (max) most recent user statuses (tweets/retweets)."""
+    tweets = TWITTER.get_user(user).timeline(count=count)
     return [tweet.text for tweet in tweets]
+
+
+def tweet_embeddings(tweets):
+    """Return the 768-dimensional embeddings for an iterable of user tweets."""
+    return [BASILICA.embed_sentence(tweet, model='twitter') for tweet in tweets]
