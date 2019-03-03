776706ffb35694ddbd085a8abf7a305d95ddfde7
soycode <soycode@users.noreply.github.com>
2019-03-03 13:57:29 -0800

    Add user comparison prediction

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,6 +2,7 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .predict import predict_user
 from .twitter import add_or_update_user, update_all_users
 
 
@@ -31,6 +32,17 @@ def create_app():
             tweets = []
         return render_template('user.html', title=name, tweets=tweets,
                                message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = request.values['user1'], request.values['user2']
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            message = '"{}" more likely to be said by: {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2)
+        return render_template('prediction.html', title='Prediction', message=message)
 
     @app.route('/reset')
     def reset():
